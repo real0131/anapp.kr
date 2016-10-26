8466c41326cea7b62fe73fc1b56acc99b6ad7fa6
Jaeho Cho <real0131@gmail.com>
2016-10-26 15:40:04 +0900

    vote

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,16 @@
 var express = require('express');
 var router = express.Router();
 
+var url = require('url');
+var num = 0;
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.post('/vote', function (req,res,next) {
+  num++;
+  console.log("투표"+num+"번-------"+req.body.vote);
+  res.end("투표완료");
+});
 module.exports = router;
