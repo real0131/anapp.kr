fccc93b8b882313c061a98a530f36370b6a46aef
Jaeho Cho <real0131@gmail.com>
2017-01-25 20:11:57 +0900

    change public files

--- routes/index.js
+++ routes/index.js
@@ -8,9 +8,4 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-router.post('/vote', function (req,res,next) {
-  num++;
-  console.log("투표"+num+"번-------"+req.body.vote);
-  res.end("투표완료");
-});
 module.exports = router;
