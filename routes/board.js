bb17492a0ef19f548971c4e9b181dfe565da9cfe
Jaeho Cho <real0131@gmail.com>
2017-02-15 22:12:36 +0900

    add views board.ejs,notice.ejs

--- routes/board.js
+++ routes/board.js
@@ -1,3 +1,10 @@
-/**
- * Created by chojeaho on 2017-02-15.
- */
+var express = require('express');
+var router = express.Router();
+
+var url = require('url');
+
+router.get('/', function(req, res, next) {
+    res.render('board');
+});
+
+module.exports = router;
