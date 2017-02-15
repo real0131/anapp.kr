c949113f8a43c9c7c730dd0290c36e3da6185755
Jaeho Cho <real0131@gmail.com>
2017-02-16 02:26:26 +0900

    add side-nav and add event

--- public/js/main.js
+++ public/js/main.js
@@ -5,3 +5,14 @@ $('.project-card').click(function () {
 $(".project-card[click='clicked']").click(function () {
    $(this).removeAttr('click');
 });
+
+$("#side-menu-btn").click(function () {
+   var width = $('.side-nav-table').width();
+    console.log(width);
+   $(".side-nav").css({display : '-webkit-flex',display : 'flex', zIndex : '200'});
+    //TODO:add animation
+});
+
+$("#side-nav-menu-cancel-btn").click(function () {
+    $(".side-nav").css({display : 'none'});
+});
