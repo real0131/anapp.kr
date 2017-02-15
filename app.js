91ef0152f61eb32b17a8d9849f7356deba65de09
Jaeho Cho <real0131@gmail.com>
2017-02-15 22:06:28 +0900

    add routes board ,notice

--- app.js
+++ app.js
@@ -26,6 +26,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', routes);
 app.use('/users', users);
 app.use('/admin', admin);
+app.use('/board',board);
+app.use('/notice',notice);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ routes/notice.js
@@ -0,0 +1,3 @@
+/**
+ * Created by chojeaho on 2017-02-15.
+ */
