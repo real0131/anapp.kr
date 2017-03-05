1cb8cec3ee558be47ce490b703a46a41994e4736
Jaeho Cho <real0131@gmail.com>
2017-03-06 03:53:26 +0900

    add join button

--- public/js/main.js
+++ public/js/main.js
@@ -11,40 +11,70 @@ function startScroll(){
 }
 
 function carouselTransition(direction,target){
+    console.log(direction);
+    console.log(target);
     var index = target.index(".item");
     var lastIndex = $(".item:nth-last-child(1)").index(".item");
     var width = target.width();
     console.log(index);
     if (direction =="left"){
+        console.log("it is left");
         //render previous item at target's left position
         //if the target's index is first render last item
         //translate +100%
         //change active status
         //change carousel-indicator
         if(index != 0){
-            $(".item:nth-child("+(index-1)+")")
-                .css({left: "-100%"})
+            /* TODO:TEST */
+            console.log("asdf");
+            /*************/
+            $(".item:nth-child("+(index)+")")
                 .addClass("active");
-            $(".item:nth-child("+(index-1)+"), .item:nth-child("+(index)+")")
-                .animate({transform: "translate(100%,0px)"},500,function () {
-                    $(".item:nth-child("+(index)+")").removeClass("active");
-            });
-
+            $(".item:nth-child("+(index)+"), .item:nth-child("+(index+1)+")")
+                .addClass(".item-anim.left");
+            $(".indicator-item.active").removeClass("active");
+            $(".indicator-item:nth-child("+ (index) +")").addClass("active");
+            $(".item:nth-child("+(index+1)+")").removeClass("active");
+            $(".item:nth-child("+(index)+"), .item:nth-child("+(index+1)+")")
+                .removeClass(".item-anim.left");
         }else{
             $(".item:nth-last-child(1)")
-                .css({left: "-100%"})
                 .addClass("active");
-            $(".item:nth-last-child(1), .item:nth-child("+(index)+")")
-                .animate({transform: "translate(100%,0px)"},500,function () {
-                    $(".item:nth-child("+(index)+")").removeClass("active");
-                });
+            $(".item:nth-last-child(1), .item:nth-child("+(index+1)+")")
+                .addClass(".item-anim.left");
+            $(".indicator-item.active").removeClass("active");
+            $(".indicator-item:nth-last-child(1)").addClass("active");
+            $(".item:nth-child("+(index+1)+")").removeClass("active");
+            $(".item:nth-last-child(1), .item:nth-child("+(index+1)+")")
+                .removeClass(".item-anim.left");
         }
     }else if (direction =="right"){
-        if(index != lastIndex){}
+        console.log("it is right");
         //render next item at target's right position
         //if the target's index is last render first item
         //position left -100
         //change carousel-indicator
+        if(index != lastIndex){
+            $(".item:nth-child("+(index+2)+")")
+                .addClass("active");
+            $(".item:nth-child("+(index+2)+"), .item:nth-child("+(index+1)+")")
+                .addClass(".item-anim.right");
+            $(".indicator-item.active").removeClass("active");
+            $(".indicator-item:nth-child("+ (index+2) +")").addClass("active");
+            $(".item:nth-child("+(index+1)+")").removeClass("active");
+            $(".item:nth-child("+(index+2)+"), .item:nth-child("+(index+1)+")")
+                .removeClass(".item-anim.right");
+        }else{
+            $(".item:nth-child(1)")
+                .addClass("active");
+            $(".item:nth-child(1), .item:nth-child("+(index+1)+")")
+                .addClass(".item-anim.right");
+            $(".indicator-item.active").removeClass("active");
+            $(".indicator-item:nth-child(1)").addClass("active");
+            $(".item:nth-child("+(index+1)+")").removeClass("active");
+            $(".item:nth-child(1), .item:nth-child("+(index+1)+")")
+                .removeClass(".item-anim.right");
+        }
     }
 }
 
@@ -79,7 +109,7 @@ $("#side-nav-menu-cancel-btn").click(function () {
     });
     startScroll();
 });
-
+/*
 $(".carousel-button.left").click(function () {
     var active = $(".item.active").index(".item");
     console.log(active);
@@ -107,6 +137,14 @@ $(".carousel-button.right").click(function () {
         $(".item:nth-child(1)").addClass("active");
         $(".indicator-item:nth-child(1)").addClass("active");
     }
+});
+*/
+$(".carousel-button.left").click(function () {
+    carouselTransition("left",$(".item.active"));
+});
+
+$(".carousel-button.right").click(function () {
+    carouselTransition("right",$(".item.active"));
 });
 
 /* smooth scroll */
